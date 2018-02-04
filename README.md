63fcb1c5230a1d572aaa6e1e9567851d191e8d61
onoie <onoie3@gmail.com>
2018-02-05 01:00:15 +0900

    add sample

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cnh
+* github.com/andelf/go-curl
+* github.com/mgood/go-jq
--- main.go
+++ main.go
@@ -7,9 +7,22 @@ import (
 )
 import (
 	"fmt"
+	"github.com/andelf/go-curl"
 )
 
 func main() {
-	fmt.Println("HelloWorld")
+	easy := curl.EasyInit()
+	defer easy.Cleanup()
+	url:="https://api.bitflyer.jp/v1/getmarkets"
+	easy.Setopt(curl.OPT_URL, url)
+	fooTest := func (buf []byte, userdata interface{}) bool {
+		println("DEBUG: size=>", len(buf))
+		println("DEBUG: content=>", string(buf))
+		return true
+	}
+	easy.Setopt(curl.OPT_WRITEFUNCTION, fooTest)
+	if err := easy.Perform(); err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+	}
 	goz.Complete()
 }
